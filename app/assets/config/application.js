8e0f82a2837f768b35eea2b07e5c6866a1c52be5
melons23 <isdom007@icloud.com>
2024-06-18 08:39:59 +0900

    0618不具合修正ver

--- app/assets/config/application.js
+++ app/assets/config/application.js
@@ -1,2 +1,7 @@
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
-pin "popper", to: 'popper.js', preload: true
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+//= require jquery_ujs
+
+import "@hotwired/turbo-rails"
